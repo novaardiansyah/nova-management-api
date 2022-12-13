1342f5b2b8ea106b980fe30eb7f7d56f933953d7
novaardiansyah <novaardiansyah78@gmail.com>
2022-12-13 16:49:16 +0700

    Improved access token validation issue

--- application/modules/v1/models/CustomerService_api.php
+++ application/modules/v1/models/CustomerService_api.php
@@ -16,7 +16,12 @@ class CustomerService_api extends CI_Model
 
   public function contactUsList($data = [])
   {
-    $result = $this->db->query("SELECT a.id, a.idUser, a.name, a.email, a.phone, a.message, a.isUser, a.isActive FROM contact_us AS a")->result();
+    $validateToken = validateAccessToken();
+    if ($validateToken !== true) return $validateToken;
+    
+    $limit = trim(isset($data['limit']) ? $data['limit'] : 10);
+
+    $result = $this->db->query("SELECT a.id, a.idUser, a.name, a.email, a.phone, a.message, a.isUser, a.isActive FROM contact_us AS a LIMIT $limit")->result();
 
     if (empty($result)) return responseModelFalse('Maaf, belum ada data yang tersedia.', 'KFTVH');
 
@@ -34,37 +39,30 @@ class CustomerService_api extends CI_Model
 
   public function factoryContactUs($data = [])
   {
-    $headers       = getallheaders();
-    $authorization = isset($headers['Authorization']) ? $headers['Authorization'] : '';
-    $token         = $authorization ? create_array($authorization, ' ')[1] : 'invalid-token';
-    
-    try {
-      JWT::decode($token, new Key($_ENV['ACCESS_TOKEN'], 'HS256'));
+    $validateToken = validateAccessToken();
+    if ($validateToken !== true) return $validateToken;
 
-      $faker = Faker\Factory::create();
+    $faker = Faker\Factory::create();
 
-      $batch = [];
+    $batch = [];
 
-      for ($i = 1; $i < 500; $i++) {
-        $send = [
-          'name'       => $faker->name,
-          'email'      => $faker->email,
-          'phone'      => $faker->phoneNumber,
-          'message'    => $faker->text,
-          'isUser'     => 0,
-          'isActive'   => 1,
-          'created_by' => 1
-        ];
+    for ($i = 1; $i < 6500; $i++) {
+      $send = [
+        'name'       => $faker->name,
+        'email'      => $faker->email,
+        'phone'      => $faker->phoneNumber,
+        'message'    => $faker->text,
+        'isUser'     => 0,
+        'isActive'   => 1,
+        'created_by' => 1
+      ];
 
-        array_push($batch, $send);
-      }
-
-      $this->db->truncate('contact_us');
-      $this->db->insert_batch('contact_us', $batch);
-
-      return responseModelTrue('Successfully generated temporary data.', ['result' => $batch]);
-    } catch (Exception $error) {
-      return responseModelFalse('Invalid access token.', 'KFTKH', ['access-token' => $token]);
+      array_push($batch, $send);
     }
+
+    $this->db->truncate('contact_us');
+    $this->db->insert_batch('contact_us', $batch);
+
+    return responseModelTrue('Successfully generated temporary data.', ['result' => $batch]);
   }
 }
